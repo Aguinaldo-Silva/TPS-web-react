2bce116ac200d07f56ef50156d360bed72480f2a
Aguinaldo Silva <aguinaldo.silva@dgt.com.br>
2024-11-04 17:17:17 -0300

    adicionando botoes

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@mui/material';
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
     const [mensagem, setMensagem] = useState<string>(' Home!');
@@ -13,6 +15,16 @@ const Home: React.FC = () => {
 
     return (
         <div>
+
+            <nav>
+                <Link to="/"><Button>Home</Button></Link>
+                <Link to="/Settings"><Button>Settings</Button></Link>
+                <Link to="/Dashboard"><Button>Dashboard</Button></Link>
+                <Link to="/SignIn"><Button>SignIn</Button></Link>
+                <Link to="/SignUp"><Button>SignUp</Button></Link>
+                <Link to="/Form"><Button>Form</Button></Link>
+
+            </nav>
             <h1>{mensagem}</h1>
             <p>Esta é a estrutura inicial da tela Home.</p>
         </div>
